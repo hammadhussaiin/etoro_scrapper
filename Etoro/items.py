6c47471f474e389b0c9d0a62303a5e0206618dea
Saad Waqar <saadwaqarmughal@gmail.com>
2017-06-08 05:13:21 +0500

    Etoro Scraper v1.02

--- Etoro/items.py
+++ Etoro/items.py
@@ -1,14 +1,11 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class EtoroItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    username = scrapy.Field()
+    profile_url = scrapy.Field()
+    performance_yearly = scrapy.Field()
+    trading_stats = scrapy.Field()
+    additional_stats = scrapy.Field()
+    items = scrapy.Field()
+
--- Etoro/main.py
+++ Etoro/main.py
@@ -1,3 +1,3 @@
 from scrapy import cmdline
-cmdline.execute("scrapy crawl etoro".split())
+cmdline.execute("scrapy crawl etoro -o etoro_data.json -t json".split())
 #cmdline.execute("scrapy crawl osray -o osray_items.json -t json".split())
